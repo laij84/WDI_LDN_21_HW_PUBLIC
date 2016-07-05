d32da44482434c23eb7d909cb32a48e9332130e0
Jason Lai <jase_lai@hotmail.com>
2016-07-05 20:19:14 +0100

    working soundboard with random play button

--- laij84/w02d02/main.js
+++ laij84/w02d02/main.js
@@ -3,6 +3,7 @@ console.log("js loaded");
 var daftPunk = {
 
   getSquares: document.querySelectorAll(".square"),
+  getBtn: document.getElementById("randomBtn"),
   
   clickListener: function() {
     for (var i = 0; i < daftPunk.getSquares.length; i++) {
@@ -12,6 +13,21 @@ var daftPunk = {
       });
     }
   },
-};
+
+//Bonus feature to randomly play one of the 16 sounds.
+  playRandom: function() {
+    var randomIndex = Math.floor(Math.random() * daftPunk.getSquares.length);
+    var randomId = daftPunk.getSquares[randomIndex].id;
+    var playRandom = new Audio("sounds/"+randomId+".wav");
+    playRandom.play();
+
+    },
+    
+  };
 
 daftPunk.clickListener();
+
+daftPunk.getBtn.addEventListener("click", function(){
+  daftPunk.playRandom(); 
+  console.log("clicked");});
+
