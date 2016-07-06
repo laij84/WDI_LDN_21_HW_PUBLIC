214881b86cbcf5f1cfeff963d5365a4d5833efef
Jason Lai <jase_lai@hotmail.com>
2016-07-06 17:10:43 +0100

    appending tweets working

---
+++ laij84/w02d03/starter-code/js/app.js
@@ -0,0 +1,45 @@
+
+// {
+// "user_thumbnail":"http://pbs.twimg.com/profile_images/589705612864331777/rZPuXSEA_normal.jpg",
+// "created_at":"2015-10-07 14:58:34 +0000",
+// "text":"Freehold - Responsive Real Estate Theme http://t.co/e7QnameF8I ",
+// "name":"Trends 2015",
+// "screen_name":"trendbisnis2015"
+// }
+
+//loop through array of objects
+$(function(){
+
+  // $(window.tweets).each(function(i){
+
+  //   var $userThumbnail = window.tweets[i].user_thumbnail;
+  //   var $createdAt = window.tweets[i].created_at;
+  //   var $tweetText = window.tweets[i].text;
+  //   var $userName = window.tweets[i].name;
+  //   var $screenName = window.tweets[i].screen_name;
+
+  //   // console.log($screenName);
+
+  //   $('.stream-items').append(
+
+  //       '<div class="tweet">' +
+  //         '<a href="#">' +
+  //           '<img src="' + $userThumbnail + '" alt="User image goes here.">'+
+  //         '</a>'+
+  //         '<div class="content">'+
+  //           '<strong class="fullname">'+$userName+'</strong>'+
+  //           '<span>&rlm; </span>'+
+  //           '<span>@</span><b>'+$screenName+'</b>'+
+  //           '&nbsp;&middot;&nbsp;'+
+  //           '<small class="time">'+
+  //             $createdAt+
+  //           '</small>'+
+  //          '<p>'+$tweetText+'</p>'+
+  //         '</div>'+
+  //       '</div>'+
+  //     '</li>'
+  //   );
+
+  // });
+
+});
