6c4a55e92d1853dd3fe1a136a39521c9a5e4cec8
Jason Lai <jase_lai@hotmail.com>
2016-07-17 13:22:28 +0100

    ejs tags pulling through array of objects data to index

--- laij84/w03d05/data/homeworkData.js
+++ laij84/w03d05/data/homeworkData.js
@@ -1,3 +1,12 @@
-var homeworkArray = [{}];
+var homeworkArray = [
+  { id: 0,
+    image: "hello",
+    caption: "Test caption"
+  },
+  { id: 0,
+    image: "Cats",
+    caption: "Hi how are you"
+  }
+];
 
 module.exports = homeworkArray;
