598a9e074fa12cbd577ed95ee2c79545d7a0b543
Jason Lai <jase_lai@hotmail.com>
2016-07-17 16:24:02 +0100

    bootstrap added to display data objects in thumbnails

--- laij84/w03d05/data/homeworkData.js
+++ laij84/w03d05/data/homeworkData.js
@@ -1,15 +1,23 @@
 var homeworkArray = [
   { id: 0,
-    image: "http://i.imgur.com/kFJ9H7u.jpg",
-    caption: "hello"
+    title: "Cat in Red Coat",
+    image: "http://www.kittenswhiskers.com/wp-content/uploads/sites/48/2014/09/cat-in-red-coat.jpg",
+    caption: "Little red riding hood as a cat."
   },
   { id: 1,
-    image: "http://i.imgur.com/DDCK3Zf.jpg",
-    caption: "Meow Meow"
+    title: "Cat in Parka",
+    image: "http://pinxpets.com/wp-content/uploads/2013/12/warmkitty-1387023088g4n8k.jpg",
+    caption: "Ready for winter."
   },
   { id: 2,
+    title: "test3",
     image: "http://i.imgur.com/OqLz8Bv.jpg",
     caption: "Dogs"
+  },
+  { id: 3,
+    title: "Cat in Rain Poncho",
+    image: "http://pinxpets.com/wp-content/uploads/2013/12/catrainjacket-13870233104nk8g.jpg",
+    caption: "Fashionable and functional."
   }
 
 ];
