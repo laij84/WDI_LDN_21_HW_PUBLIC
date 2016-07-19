6b818ce09f93505ef022ebfdc86c26ed25a5e1af
Jason Lai <jase_lai@hotmail.com>
2016-07-19 16:47:35 +0100

    working with player hand for jack queen king

--- laij84/w04d02/main.rb
+++ laij84/w04d02/main.rb
@@ -1,4 +1,4 @@
-cards = [1,2,3,4,5,6,7,8,9,10]
+cards = [1,2,3,4,5,6,7,8,9,10, "Jack", "Queen", "King"]
 suits = ["♠", "♣", "♥", "♦"] 
 
 deck = cards.product(suits).shuffle
@@ -9,17 +9,14 @@ deck = cards.product(suits).shuffle
 player_array = deck.pop(2)
 dealer_array = deck.pop(2)
 
-
-puts "Player's hand #{player_array}"
-
 puts "Player's hand contains #{player_array[0][0]}#{player_array[0][1]} and the #{player_array[1][0]}#{player_array[1][1]}"
 
 player_array.reduce do |prev, current|
-  # if prev[0] == "Jack" || "Queen" || "King"
-  #    prev[0] = 10
-  # elsif current[0] == "Jack" || "Queen" || "King"
-  #    current[0] = 10
-  # end
-  total = prev[0]+current[0] 
-  puts total
+  if prev[0] == "Jack" || prev[0] == "Queen" || prev[0] == "King"
+     prev[0] = 10
+  elsif current[0] == "Jack" || current[0] ==  "Queen" || current[0] ==  "King"
+     current[0] = 10 
+  end
+  total = prev[0].to_i+current[0].to_i 
+puts total
 end
