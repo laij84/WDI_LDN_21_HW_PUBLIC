f24a2573476bfaed5f6d62532d4e515322a32467
Jason Lai <jase_lai@hotmail.com>
2016-07-27 18:57:25 +0100

    edit/update/delete working, css added

--- laij84/w05d03/controllers/videos.rb
+++ laij84/w05d03/controllers/videos.rb
@@ -21,3 +21,23 @@ get '/videos/:id' do
   @video = Video.find(params[:id])
   erb :"videos/show"
 end
+
+#EDIT
+get '/videos/:id/edit' do
+  @video = Video.find(params[:id])
+  erb :"videos/edit"
+end
+
+#UPDATE
+put '/videos/:id' do
+  @video = Video.find(params[:id])
+  @video.update_attributes(params[:video])
+  redirect "/videos/#{@video.id}"
+end
+
+#DELETE
+delete '/videos/:id' do 
+  video = Video.find(params[:id])
+  video.destroy
+  redirect '/videos'
+end
