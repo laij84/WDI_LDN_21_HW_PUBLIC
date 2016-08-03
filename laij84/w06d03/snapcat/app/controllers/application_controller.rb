6aed0f728af2c5eae7e32ec95c302a9496144fbf
Jason Lai <jase_lai@hotmail.com>
2016-08-03 20:53:58 +0100

    i give up

--- laij84/w06d03/snapcat/app/controllers/application_controller.rb
+++ laij84/w06d03/snapcat/app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
+
+    protected
+
+    def after_sign_in_path_for(resource)
+      posts_path
+    end
+
+
+    def configure_permitted_parameters      
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name])
+    end
+
+    def after_sign_in_path_for(resource)
+      posts_path
+    end
+
   protect_from_forgery with: :exception
 end
