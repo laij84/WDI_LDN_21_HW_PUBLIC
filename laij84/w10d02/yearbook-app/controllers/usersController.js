dc3000180a0bd6e075c0d72c4805753dab342cb3
Jason Lai <jase_lai@hotmail.com>
2016-08-30 18:39:47 +0100

    CRUD working

--- laij84/w10d02/yearbook-app/controllers/usersController.js
+++ laij84/w10d02/yearbook-app/controllers/usersController.js
@@ -15,17 +15,75 @@ function usersController($http, API_URL) {
     });
   })(); //self-invoking function
 
+  this.addUser = addUser;
+  this.newUser = {};
+
+  function addUser() {
+
+    $http
+      .post(API_URL, self.newUser)
+      .then(function(response){
+        console.log(response);
+        self.all.push(response.data);
+        self.newUser = {};
+        self.newForm = false;
+      });
+  }
+
+  this.toggleNewForm = toggleNewForm;
+  this.newForm = false;
+
+  function toggleNewForm() {
+    self.newForm = !self.newForm;
+    console.log("Clicked new form");
+  }
+
+
   this.selectUser = selectUser;
-  this.selectedUser = null;
+  self.selectedUser = null;
 
   function selectUser(user){
-    self.selectedUser = user;
-    console.log(self.selectedUser);
+    self.selectedUser = angular.copy(user);
+    self._selectedUser = user;
   }
 
   this.resetUser = resetUser;
   function resetUser(){
     self.selectedUser = null;
+  }
+
+  this.toggleEditForm = toggleEditForm;
+  this.editForm = false;
+
+  function toggleEditForm() {
+    self.editForm = !self.editForm;
+    console.log("CLICKED");
+  }
+
+  this.updateUser = updateUser;
+
+  function updateUser(){
+    $http
+      .put(API_URL+"/"+self.selectedUser._id, self.selectedUser)
+      .then(function(response){
+        for(key in response.data){
+         self._selectedUser[key] = response.data[key];
+        }
+        self.editForm = false;
+      });
+  }
+
+  this.deleteUser = deleteUser;
+
+  function deleteUser(user) {
+    $http
+      .delete(API_URL+"/"+user._id)
+      .then(function(response){
+        console.log("delete!")
+        var index = self.all.indexOf(user);
+        self.all.splice(index,1);
+        self.selectedUser = null;
+      });
   }
 
 }
